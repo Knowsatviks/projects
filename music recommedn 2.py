e204a781310efe6cbc26146aa4767865e6875bca
Knowsatviks <95932478+Knowsatviks@users.noreply.github.com>
2023-07-17 16:15:08 +0530

    Update music recommedn 2.py

--- music recommedn 2.py
+++ music recommedn 2.py
@@ -17,15 +17,15 @@ def clear(*args):
     args.destroy()
 def display_recommendations(response):
     lbl_track_name= tk.Label(master= frm_recommendations, text = 'Track Name')
-    lbl_track_name= tk.Label(master= frm_recommendations, text = 'Artist Name')
-    lbl_track_name= tk.Label(master= frm_recommendations, text = 'Play It')
+    lbl_artist_name= tk.Label(master= frm_recommendations, text = 'Artist Name')
+    lbl_play_it= tk.Label(master= frm_recommendations, text = 'Play It')
     lbl_track_name.grid(row=0, column=0)
     lbl_artist_name.grid(row=0, column=1)
     lbl_play_it.grid(row=0, column=2)
-    for idx, track in enumerate(response['track']):
+    for idx, track in enumerate(response['tracks']):
         lbl_track_name_recommended = create_label(track['name'])
         lbl_track_name_recommended.grid(row= idx+1, column = 0)
-        lbl_artist_name_recommended= create_label(track['artist'][0]['name'])
+        lbl_artist_name_recommended= create_label(track['artists'][0]['name'])
         lbl_artist_name_recommended.grid(row=idx+1, column =1)
         btn_play_it_recommended = create_button('Play It', track['external_urls']['spotify'])
         btn_play_it_recommended.grid(row= idx+1, column=2, padx=10)
@@ -33,7 +33,8 @@ def display_recommendations(response):
 def get_recommendations():
     search = ent_search.get()
     sp= spotipy.Spotify(client_credentials_manager = SpotifyClientCredentials("366d4ba51d7c421581c1ffb26d90d44b","ebc35744ce2f46ef875ae7b32f33ed90"))
-    result= sp.search(q= search, limit=1)
+    result= sp.search(q= search, limit=2)
+    
     id_list=[result['tracks']['items'][0]['id']] 
     recommendations= sp.recommendations(seed_tracks = id_list, limit = 10)
     display_recommendations(recommendations)
@@ -48,4 +49,3 @@ btn_get_recommendations = tk.Button(master = frm_search_field, text ='Get recomm
 ent_search.grid(row = 0, column = 0, pady= 10, padx = 10) 
 btn_get_recommendations.grid(row=0, column=1, pady= 10, padx= 10)
 window.mainloop()
-
